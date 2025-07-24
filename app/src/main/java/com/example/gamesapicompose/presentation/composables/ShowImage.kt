7604e368a7bc1e9e3ae95d89075ec390ccabd910
Erick-Avila <erick_avila434@hotmail.com>
2025-07-24 16:15:51 -0600

    Task 9 (Improvements) - Validations in UI
    Validaciones actualizadas para mostrar datos.

--- app/src/main/java/com/example/gamesapicompose/presentation/composables/ShowImage.kt
+++ app/src/main/java/com/example/gamesapicompose/presentation/composables/ShowImage.kt
@@ -7,27 +7,38 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import coil.compose.rememberImagePainter
 import com.example.gamesapicompose.R
 
 @Composable
-fun ShowImage(image: String) {
-    val videoGameCover = rememberImagePainter(
-        data = image,
-        builder = {
-            placeholder(R.drawable.icon_image_placeholder)
-            error(R.drawable.icon_image_not_found)
-            fallback(R.drawable.icon_image_not_found)
-        }
-    )
-
-    Image(
-        painter = videoGameCover,
-        contentDescription = stringResource(R.string.videogame_cover),
-        contentScale = ContentScale.Crop,
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(dimensionResource(id = R.dimen.size_250dp))
-    )
+fun ShowImage(image: String?) {
+    if (!image.isNullOrEmpty()){
+        val videoGameCover = rememberImagePainter(
+            data = image,
+            builder = {
+                placeholder(R.drawable.icon_image_placeholder)
+                error(R.drawable.icon_image_not_found)
+                fallback(R.drawable.icon_image_not_found)
+            }
+        )
+        Image(
+            painter = videoGameCover,
+            contentDescription = stringResource(R.string.videogame_cover),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(dimensionResource(id = R.dimen.size_250dp))
+        )
+    }else{
+        Image(
+            painter = painterResource(R.drawable.icon_image_not_found),
+            contentDescription = stringResource(R.string.videogame_cover),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(dimensionResource(id = R.dimen.size_250dp))
+        )
+    }
 }
